017726046df5dc01c1d6882ed9f84774d5396a71
jay <joswxx@gmail.com>
2023-11-20 22:41:12 +0900

    API 요청 실패시.

--- src/main/java/com/example/cryptopulseplay/infrastructure/api/PriceClient.java
+++ src/main/java/com/example/cryptopulseplay/infrastructure/api/PriceClient.java
@@ -1,11 +1,15 @@
 package com.example.cryptopulseplay.infrastructure.api;
 
 
+import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @Component
+@Slf4j
 public class PriceClient {
 
     private final WebClient webClient;
@@ -16,11 +20,15 @@ public class PriceClient {
 
 
     public Mono<Double> getBtcPrice() {
-        return webClient.get()
+        return
+        webClient.get()
                 .uri("/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
                 .retrieve()
                 .bodyToMono(BtcPriceResponse.class)
-                .map(response -> response.bitcoin.usd);
+                .map(response -> response.bitcoin.usd)
+                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(10)))
+                .doOnSuccess( p -> log.info("success"))
+                .doOnError(e -> log.error("error", e));
     }
 
     private static class BtcPriceResponse{
